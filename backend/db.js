2b5bf534622c1c8aabf860ec26ca2c7e97f38e92
Nakiti <nikhilakiti91@gmail.com>
2023-04-14 09:05:59 -0700

    mash25

--- backend/db.js
+++ backend/db.js
@@ -9,19 +9,19 @@ let config = {
   port: 3306,
   // ssl: {ca: fs.readFileSync(path.resolve("../DigiCertGlobalRootCA.crt.pem"))}
 }
-export const db = new mysql.createConnection(config)
+export const db = new mysql.createPool(config)
 
-db.connect(
-  function (err) { 
-  if (err) { 
-    console.log("!!! Cannot connect !!! Error:");
-    throw err;
-  }
-  else
-  {
-    console.log("Connection established.");
-  }
-});
+// db.connect(
+//   function (err) { 
+//   if (err) { 
+//     console.log("!!! Cannot connect !!! Error:");
+//     throw err;
+//   }
+//   else
+//   {
+//     console.log("Connection established.");
+//   }
+// });
 
 
 
