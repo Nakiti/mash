66290099293783c35d874d3eefcbfa9ec3c56597
Nakiti <nikhilakiti91@gmail.com>
2023-04-14 09:04:39 -0700

    mash24

--- backend/index.js
+++ backend/index.js
@@ -20,9 +20,9 @@ app.use(cors(corsOptions))
 
 const __dirname = path.resolve()
 
-app.use(express.static(path.join(__dirname, ".././fronted/build")));
+app.use(express.static(path.join(__dirname, "/build")));
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, ".././frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "/build", "index.html"));
 });
 
 app.use("/auth", authRoutes)
