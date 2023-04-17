ee497a1772d76f444fd6a530b95f98e79ff4b9a9
Nakiti <nikhilakiti91@gmail.com>
2023-04-16 20:20:44 -0700

    mash72

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -27,7 +27,7 @@ const Search = () => {
     const getData = async() => {
       try {
         const response = await axios.get("/mashes/getmashbycat/all")
-        setOg(response.data)
+        setOg(response.data.reverse())
 
         const temp = [...response.data]
         setCards(temp.sort((a, b) => b.plays - a.plays))
