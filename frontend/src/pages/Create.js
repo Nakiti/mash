5cc01e080f41f36f29d48ee31abcace7cfe6892c
Nakiti <nikhilakiti91@gmail.com>
2023-04-27 20:47:09 -0700

    mash104

--- frontend/src/pages/Create.js
+++ frontend/src/pages/Create.js
@@ -217,13 +217,13 @@ const Create = () => {
             <p className="create-label">Info:  </p>
             <input type="text" className="create-info create-input" placeholder="Add Some Context, Give Image Credits, etc." onChange={(e) => setInfo(e.target.value)}/>
           </div>
-          <div className="create-group create-ques-group">
+          {/* <div className="create-group create-ques-group">
             <p className="create-label">Question:  </p>
             <select className="create-switch create-input" onChange={(e) => setQues(e.target.value)}>
               <option value="Which One's Better?">Which One's Better?</option>
               <option value="Which Do You Prefer?">Which Do You Prefer?</option>
             </select>
-          </div>
+          </div> */}
         </div>
         <div className="create-line" />
         <div className="create-form">
