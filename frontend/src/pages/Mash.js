8cd099eb536a42cde02d5185eb3ae5b9d4c0ca30
Nakiti <nikhilakiti91@gmail.com>
2023-07-26 12:08:14 -0700

    mash136

--- frontend/src/pages/Mash.js
+++ frontend/src/pages/Mash.js
@@ -62,7 +62,7 @@ const Mash = () => {
             // console.log(userCards[one], userCards[two])
             break
         } else {
-          if (rand > 75 && topPairs.length > 0 && mashPlays > 1000) {
+          if (rand > 90 && topPairs.length > 0 && mashPlays > 1000) {
             const temp= [...topPairs]
             let pair =  temp[Math.floor(Math.random() * temp.length)]
 
